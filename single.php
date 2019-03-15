04ff1398ca0dab870f2bdace19348aa92a94b63c
Mauro Bono <bono.mauro@gmail.com>
2019-03-15 14:59:36 +0000

    added get_post_format() and content-gallery.php

--- single.php
+++ single.php
@@ -6,7 +6,7 @@
             
             <?php if ( have_posts() ) : while ( have_posts() ) : the_post()  ?>
             
-                <?php get_template_part( 'template-parts/content' ); ?>
+                <?php get_template_part( 'template-parts/content', get_post_format() ); ?>
             
             <?php endwhile; else : ?>
             
--- template-parts/content-gallery.php
+++ template-parts/content-gallery.php
@@ -3,8 +3,9 @@
     <header class="entry-header">
         
         <span class="dashicons dashicons-format-<?php echo get_post_format( $post->ID ); ?>">
-            
         </span>
+        
+        <p><?php esc_html_e( 'Enjoy this gallery post', 'anoobi' ); ?></p>
         
         <?php the_title( '<h1>', '</h1>' ); ?>
         
